7b599c4547cbbcecece4f007db247aec43aca2e6
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-11-28 15:17:27 +0200

    Creating a notification function

--- Notify/app/Http/Controllers/NotificationController.php
+++ Notify/app/Http/Controllers/NotificationController.php
@@ -4,7 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Notifications\NewOrders;
+
 class NotificationController extends Controller
 {
-    //
+    public function sendNotification()
+    {
+        $user = "mribrahim@mail.com";
+        $order = [
+            'greeting' => 'Hello Mr Ibrahim',
+            'body' => 'A new order has been placed',
+            'lastline' => 'Thank you'
+        ];
+        Order::send($order, new NewOrders($order));
+        return back()->with('success', 'Notification sent successfully');
+    }
 }
